9c236aafe893dd74a5d6094f62fba05e9c88b208
Illuminatus66 <varunsodhani123@gmail.com>
2024-12-13 15:37:02 +0530

    final commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://inmaco_backend(placeholder).com",
+  baseURL: "https://inmaco-backend.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
